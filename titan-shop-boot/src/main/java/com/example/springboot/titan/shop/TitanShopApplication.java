c6792cba3dd65c1f1129bb30a0d9dcb7e6518c48
eric.mo <mozs@xiaopeng.com>
2020-03-09 09:12:27 +0800

    - Added 引入 Ribbon 负载均衡

--- titan-shop-boot/src/main/java/com/example/springboot/titan/shop/TitanShopApplication.java
+++ titan-shop-boot/src/main/java/com/example/springboot/titan/shop/TitanShopApplication.java
@@ -2,6 +2,7 @@ package com.example.springboot.titan.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +13,9 @@ public class TitanShopApplication {
         SpringApplication.run(TitanShopApplication.class, args);
     }
 
+    //负载均衡
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- titan-shop-boot/src/main/java/com/example/springboot/titan/shop/webapp/ShopController.java
+++ titan-shop-boot/src/main/java/com/example/springboot/titan/shop/webapp/ShopController.java
@@ -4,6 +4,8 @@ import com.example.springboot.titan.shop.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +25,31 @@ public class ShopController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/user/{userId}")
-    public User getUser(@PathVariable String userId) {
-        log.info("/getUser, userId: " + userId);
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/user2/{userId}")
+    public User getUser2(@PathVariable String userId) {
+        log.info("/user2/" + userId);
 
         return restTemplate.getForObject(userGetUserUrl + userId, User.class);
+    }
+
+    /**
+     * 使用 ribbon 负载均衡重新获取 用户信息
+     */
+    @GetMapping("/user/{userId}")
+    public User getUser(@PathVariable String userId) {
+        log.info("/user/" + userId);
+
+        return restTemplate.getForObject("http://titan-user-boot/findByUserId/" + userId, User.class);
+    }
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("titan-user-boot");
+        log.info("/log-user-instance 当前调用链 {}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+
     }
 }
